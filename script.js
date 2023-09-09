20ebeab71d7fd97fb9b2da1dec257880714b4519
tusharjain2004 <tushar2004jain@gmail.com>
2023-09-10 02:07:35 +0530

    about img padding bottom 20->0%

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Web Developer", "Compititive Coder"],
+    strings: ["Frontend Web Developer", "Competitive Coder", "Marketing Expert"],
     typespeed: 100,
     backspeed: 100,
     backDelay: 1000,
